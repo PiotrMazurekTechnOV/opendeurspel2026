b3e2561bb536803a0eebd94ee83b0df25182b796
rayyane12 <rayyane.lamghaouach@leerling.kov.be>
2026-01-20 15:10:15 +0100

    aanmaak userIdentification en Feedback

---
+++ vraagprogramma/vraagprogramma/vraagprogramma/feedBack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace vraagprogramma
+{
+    public partial class feedBack : Form
+    {
+        public feedBack()
+        {
+            InitializeComponent();
+        }
+    }
+}
