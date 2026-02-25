e417a831fe8e769879f80a2dd9b8b9c1ac555013
AybICT <ayoub.mahouti@leerling.kov.be>
2026-02-25 10:40:16 +0100

    Label random code

--- registratieProgramma/formulierProgramma/formulierProgramma/Code.Designer.cs
+++ registratieProgramma/formulierProgramma/formulierProgramma/Code.Designer.cs
@@ -28,13 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.CodeLabel = new CuoreUI.Controls.cuiLabel();
             this.SuspendLayout();
+            // 
+            // CodeLabel
+            // 
+            this.CodeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CodeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.CodeLabel.Content = "Random\\ Code";
+            this.CodeLabel.Font = new System.Drawing.Font("Broadway", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CodeLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.CodeLabel.HorizontalAlignment = System.Drawing.StringAlignment.Center;
+            this.CodeLabel.Location = new System.Drawing.Point(254, 77);
+            this.CodeLabel.Name = "CodeLabel";
+            this.CodeLabel.Size = new System.Drawing.Size(210, 47);
+            this.CodeLabel.TabIndex = 0;
+            this.CodeLabel.VerticalAlignment = System.Drawing.StringAlignment.Center;
             // 
             // Code
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CodeLabel);
             this.Name = "Code";
             this.Text = "Code";
             this.Load += new System.EventHandler(this.Code_Load);
@@ -43,5 +61,7 @@
         }
 
         #endregion
+
+        private CuoreUI.Controls.cuiLabel CodeLabel;
     }
 }
