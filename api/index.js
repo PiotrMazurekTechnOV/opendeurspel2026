7a65b7a17a32a705f4eff71143dfc50a0af383ef
Ill.Il.ICT <illia.iliuk@leerling.kov.be>
2026-03-03 15:28:42 +0100

    hotfix: number changed to location_number

--- api/index.js
+++ api/index.js
@@ -422,14 +422,14 @@ server.get("/locations/read/:id", async (req, res, next) => {
 });
 
 //GET locatios opvragen aan de hand van id
-server.get("/question/get/location/:number", async (req, res) => {
+server.get("/question/get/location/:location_number", async (req, res) => {
   try {
-    const { number } = req.params;
+    const { location_number } = req.params;
 
     const con = await connect();
     const query = `
-      SELECT * FROM questions 
-      WHERE number = ?
+      SELECT text FROM questions 
+      WHERE location_number = ?
     `;
     const [rows] = await con.execute(query, [number]);
     await con.end();
