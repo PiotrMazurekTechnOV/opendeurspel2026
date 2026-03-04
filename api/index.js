61e6d4d6dbce935b12131d34aa94dd9854d95b23
jibransubhani <jibran.subhani@leerling.kov.be>
2026-03-04 11:36:36 +0100

    GET ALL questions i.p.v alleen de eerste rij

--- api/index.js
+++ api/index.js
@@ -221,7 +221,7 @@ server.get("/question/get/all", async (req, res) => {
     await con.end();
 
     if (rows.length === 0) return res.status(404).json({ error: "questions not found" });
-    res.json(rows[0]);
+    res.json(rows);   // <-- hier veranderd
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
